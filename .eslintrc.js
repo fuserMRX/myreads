f24cbc3a1d33f7c8f13b0829496cd07d6d3f0fa8
fuser <vnovodid@gmail.com>
2020-01-20 10:06:36 +0200

    feat: Initial setup commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "jest": true
+    },
+    "parser": "babel-eslint",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "jsdoc"
+    ],
+    "rules": {
+        "quotes": ["error", "single"],
+        "no-trailing-spaces": [2, { "skipBlankLines": true }],
+        "indent": ["error", 4, { "SwitchCase": 1 }],
+        "semi": "error",
+        "array-bracket-spacing": ["error", "never"],
+        "no-multi-spaces": "error",
+        "react/no-unescaped-entities": 0,
+        "jsdoc/require-jsdoc": 1,
+        "jsdoc/require-param": 1,
+        "jsdoc/require-returns": 1,
+        "jsdoc/require-description": 1,
+    }
+};
